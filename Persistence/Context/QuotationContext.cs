5e4db38e4dc05b01a2eed3c38bf324567fa69483
Iacob <Stefan.Iacob@centric.eu>
2021-07-22 17:30:52 +0300

    Persistence added

---
+++ Persistence/Context/QuotationContext.cs
@@ -0,0 +1,36 @@
+﻿
+using Application.Interfaces;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace Persistence.Context
+{
+    public class QuotationContext : DbContext, IApplicationContext
+    {
+        public QuotationContext()
+        {
+        }
+
+        public QuotationContext(DbContextOptions<QuotationContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Quotation> Quotations { get; set; }
+        public DbSet<Dossier> Dossiers { get; set; }
+
+        public async Task<int> SaveChangesAsync()
+        {
+            return await base.SaveChangesAsync();
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Quotation>()
+                .HasOne(q => q.CurrentDossier)
+                .WithMany(d => d.Quotations)
+                .HasForeignKey(q => q.DossierId);
+
+        }
+    }
+}
