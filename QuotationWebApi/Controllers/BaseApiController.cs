e66d07249ae83b84369321b2d01b6bbf41a48705
Iacob <Stefan.Iacob@centric.eu>
2021-07-22 17:41:24 +0300

    presentation layer

---
+++ QuotationWebApi/Controllers/BaseApiController.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace QuotationWebApi.Controllers
+{
+    [Route("api/v{version:apiVersion}/[controller]")]
+    [ApiController]
+
+    public class BaseApiController : ControllerBase
+    {
+        protected readonly IMediator _mediatR;
+
+        public BaseApiController(IMediator mediatR)
+        {
+            _mediatR = mediatR;
+        }
+    }
+}
