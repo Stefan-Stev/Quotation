ce51f6a0c4915fd772c6f74d9299b101f93fd233
Iacob <Stefan.Iacob@centric.eu>
2021-07-22 17:19:24 +0300

    Domain Application Done

---
+++ Domain/Entities/Quotation.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text.Json.Serialization;
+
+namespace Domain.Entities
+{
+    public class Quotation
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public Guid DossierId { get; set; }
+
+
+        [JsonIgnore]
+        public Dossier CurrentDossier { get; set; }
+
+        public decimal Price { get; set; }
+
+        public Status Status { get; set; } 
+
+        public DateTime CreationDate { get; set; }
+
+        public DateTime ValidUntilDate { get; set; } 
+    }
+}
